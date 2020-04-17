7ace735793056a087e4431c19ad05391ec109a65
Josh Beglan <josh_beglan@yahoo.com>
2020-04-17 02:35:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  
+  pigeon_list = {}
+
+  data.each do |characteristic, value|
+    value.each do |stats, all_names|
+      all_names.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][characteristic] == nil
+          pigeon_list[name][characteristic] = []
+        end
+        pigeon_list[name][characteristic].push(stats.to_s)
+      end
+    end 
+  end
+
+  pigeon_list
 end
